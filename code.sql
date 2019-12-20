a93ca0893bcc7083459c0a3353aef5c32f7ce114
BogVin <34000503+BogVin@users.noreply.github.com>
2019-12-20 12:40:15 +0200

    Update code.sql

--- code.sql
+++ code.sql
@@ -34,8 +34,11 @@ CREATE TABLE IF NOT EXISTS Bus_route(
 
 
 LOAD DATA LOCAL INFILE 'Bus_routes.txt' INTO TABLE Bus_route;
+
 LOAD DATA LOCAL INFILE 'Routes.txt' INTO TABLE Routes;
+
 LOAD DATA LOCAL INFILE 'Stations.txt' INTO TABLE Stations;
+
 LOAD DATA LOCAL INFILE 'Towns.txt' INTO TABLE Towns;
 
 
@@ -51,6 +54,7 @@ ALTER TABLE Bus_route
 ADD FOREIGN KEY (from_station) REFERENCES Stations (station_id),
 ADD FOREIGN KEY (to_station) REFERENCES Stations (station_id);
 
+
 DROP TRIGGER IF EXISTS Delete_route;
 DELIMITER $$
 CREATE TRIGGER Delete_route BEFORE DELETE  ON Routes
@@ -62,6 +66,37 @@ FOR EACH ROW
 DELIMITER ;
 
 
+DROP TRIGGER IF EXISTS Insert_chek;
+DELIMITER $$
+
+CREATE TRIGGER Insert_chek BEFORE INSERT  ON Bus_route
+FOR EACH ROW 
+ BEGIN
+	IF NEW.start_time = NEW.end_time THEN
+	SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Same time!';
+	END IF;
+ END $$
+DELIMITER;
+
+
+CREATE OR REPLACE VIEW rt as 
+select
+    route,
+    s1.town as s_first_town,
+    s1.station_name as first_station,
+	s1.station_id as s_first_id,
+	start_time,
+    s2.town as s_second_town,
+    s2.station_name as second_station,
+    s2.station_id as s_second_id,
+    end_time, distance
+from
+    Bus_route b
+    inner join Stations s1 on
+        b.from_station = s1.station_id
+    inner join Stations s2 on
+        b.to_station = s2.station_id;
 
 
 
